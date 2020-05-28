9c542172f4d9fd4b8888ca442e7a414b85cad0bc
theonionseed <tri@jitterware.com>
2020-05-28 14:26:08 -0400

    update app.py

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ def getAllSales(caseCol):
    return (np.add(np.add(salesCountOn,salesCountAb),salesCountQc))
 
 if __name__ == "__main__":
-   app.run(debug=True,host="localhost",port=5000)
+   app.run(debug=True,host="0.0.0.0",port=80)
